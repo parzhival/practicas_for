c165d226099f5236b4196d8ab3f2cf2656cc8050
parzhival <jean08070225@gmail.com>
2026-04-26 20:37:53 -0500

    feat: functions and evidence are added

--- repaso.js
+++ repaso.js
@@ -14,7 +14,16 @@ function mostrar(opcion){
             break; 
         case 5:
             mostrarMensaje2();
-            break;             
+            break; 
+        case 6:
+            mostrarMensaje3();
+            break;
+        case 7:
+            mostrarMensaje4();
+            break;
+        case 8:
+            imprimirTable3();
+            break;
         default:
             break;
     }
@@ -26,23 +35,38 @@ function ejercicio1(){
     }  
 }
 function imprimirCentenas(){
-    for (let i = 1200; i < 2400 ; i+=100){
+    for (let i = 100; i <= 1000 ; i+=100){
         console.log(i);
     } 
 }
 function imprimirCentenasRegresivo(){
-    for (let i = 1700; i > 800 ; i-=100){
+    for (let i = 1000; i >= 800 ; i-=100){
         console.log(i);
     }
 }
 
 function mostrarMensaje1(){
     for (let i= 5; i < 8; i++){
-        console.log(i);
+        console.log("tienes " + i + " años");
     }
 }
 function mostrarMensaje2(){
     for (let i= 10; i >=8; i--){
-        console.log(i);
+        console.log("tienes " + i + " centavos");
     }
 }
+function mostrarMensaje3(){
+    for (let i= 0; i < 3; i++){
+        console.log("tienes " + i + " pares");
+    }
+} 
+function mostrarMensaje4(){
+    for (let i= 8; i > 5; i--){
+        console.log("tienes " + i + " hermanos");
+    }       
+}   
+function imprimirTable3(){
+    for(let i= 1; i <= 10; i++){
+        console.log("3 x " + i + " = " + (3*i));
+    }
+}    
